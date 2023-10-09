2361824bc531841d8b5b37d7d78e84aaf9d2f913
onyxpeek <onyxpeek@gmail.com>
2023-10-09 11:29:14 +0300

    feat: add the fourth problem

--- FirstLab/fourthAppPlotDimentions.py
+++ FirstLab/fourthAppPlotDimentions.py
@@ -1,3 +1,14 @@
 import cv2
-img = cv2.imread('Lenna.png')
+img = cv2.imread('chess.jpg')
 from matplotlib import pyplot as plt
+bicubic_img = cv2.resize(img, None, fx = 2, fy = 2 , interpolation = cv2.INTER_CUBIC)
+near_img = cv2.resize(img, None, fx = 2, fy = 2, interpolation = cv2.INTER_NEAREST)
+bilinear_img = cv2.resize(img,None, fx = 2, fy = 2, interpolation = cv2.INTER_LINEAR)
+cv2.imshow("Imaginea originală", bicubic_img)
+plt.subplot(221), plt.imshow(img, 'gray'), plt.title('Imagine originala')
+plt.subplot(222), plt.imshow(bicubic_img , 'gray'), plt.title('bicubic')
+plt.subplot(223), plt.imshow(bilinear_img, 'gray'), plt.title('cel mai apropiat vecin')
+plt.subplot(224), plt.imshow(bilinear_img, 'gray'), plt.title('bilinara')
+plt.show()
+cv2.waitKey()
+cv2.destroyAllWindows()
